99503e4a0d4bc2a3e6973d96bd2800ea83223680
Michael Schoenecker <michael.schoenecker@gmail.com>
2022-06-13 12:32:17 -0400

    finish with react component sprint

--- app.js
+++ app.js
@@ -1,1 +1,44 @@
 // TODO
+
+class GroceryListItem extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      done: false
+    };
+  }
+
+  onListItemHover() {
+    this.setState({
+      done: !this.state.done
+    });
+  }
+
+  render() {
+    var style = {
+      fontWeight: this.state.done ? 'bold' : 'normal'
+    };
+
+    return (
+      <li style={style} onMouseOver={this.onListItemHover.bind(this)}>{this.props.item}</li>
+    );
+  }
+}
+
+var GroceryList = (props) => (
+  <ul>
+    {props.items.map(item =>
+      <GroceryListItem item={item} />
+    )}
+  </ul>
+);
+
+var App = () => (
+  <div>
+    <h2>Grocery List</h2>
+    <GroceryList items={['Cucumbers', 'Kale']}/>
+  </div>
+);
+
+ReactDOM.render(<App />, document.getElementById("app"));
